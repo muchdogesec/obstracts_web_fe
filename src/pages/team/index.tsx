fcad54a0fb3b71cbcb2bce6b73828600153dab59
David G <himynamesdave@users.noreply.github.com>
2025-01-13 12:51:06 +0000

    Update index.tsx

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -298,12 +298,24 @@ function Team() {
 
   return (
     <Container>
+
       <Box my={4}>
+
         <Box sx={{ display: 'flex' }}>
           <Typography sx={{ flex: 'auto' }} variant="h4" gutterBottom>
             Team Management
           </Typography>
+          <Typography className="description">
+            <p>Use this page to view information about your team. Only a team owner or a team admin can make changes.</p>
+          </Typography>
+
+          <Typography variant="h5">Team Details</Typography>
+          <Typography className="description">
+            <p>Your team ID, name, and description is only visible to members of this team and those that are invited to join the team.</p>
+          </Typography>
+
         </Box>
+
         <Grid container spacing={2}>
         </Grid>
         <Dialog open={openInvite} onClose={handleCloseInvite}>
@@ -376,11 +388,22 @@ function Team() {
             disabled={disableEdit}
           />
           {activeUserIsAdmin && disableEdit && <Button color="success" variant="contained" onClick={() => setDisableEdit(false)}>Edit</Button>}
+        
         </Box>
+        
         <Box mt={4}>
-          <Typography variant="h6" gutterBottom>
-            Subscription
+
+          <Typography variant="h5" gutterBottom>
+            Team Subscription
           </Typography>
+
+          <Typography variant="h6" gutterBottom>
+            Current Plan
+          </Typography>
+          <Typography className="description">
+            <p>This table shows details of the plan you are subscribed to. To change or cancel you can modify your subscription by clicking the button below.</p>
+          </Typography>
+
           <TableContainer component={Paper} sx={{ marginTop: '2rem' }}>
             <Table>
               <TableHead>
@@ -395,32 +418,39 @@ function Team() {
               </TableHead>
               <TableBody>
                 <TableRow>
-                  <TableCell style={{ fontWeight: 'bold' }}>Product name:</TableCell>
+                  <TableCell>Product name</TableCell>
                   <TableCell>{activeSubscription?.items[0].price.product_name}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontWeight: 'bold' }}>Cost:</TableCell>
+                  <TableCell>Cost</TableCell>
                   <TableCell>{activeSubscription?.items[0].price.payment_amount}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontWeight: 'bold' }}>Next Billing Date:</TableCell>
+                  <TableCell>Next Billing Date</TableCell>
                   <TableCell>{getSubscriptionNextBillingDate()}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontWeight: 'bold' }}>Subscription Status:</TableCell>
+                  <TableCell>Subscription Status</TableCell>
                   <TableCell sx={{ textTransform: 'capitalize' }}>{activeSubscription?.status || 'No subscription'}</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
           </TableContainer>
+
+          <Typography variant="h6" gutterBottom>
+            Plan Limits
+          </Typography>
+          <Typography className="description">
+            <p>This table shows the limits of the plan you are currently subscribed to. If you want to change these limits you can modify your subscription by clicking the button below.</p>
+          </Typography>
+
           <TableContainer component={Paper} sx={{ marginTop: '2rem' }}>
             <Table>
               <TableHead>
                 <TableRow>
-                  <TableCell>Feature</TableCell>
-                  <TableCell>Value</TableCell>
-                  <TableCell>Usage</TableCell>
-                  <TableCell></TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Feature</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Value</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Usage</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -453,21 +483,31 @@ function Team() {
             </Table>
           </TableContainer>
           {activeUserIsAdmin && <Button sx={{ marginTop: '1rem' }} onClick={handleSubscriptionDetail} variant="contained">Modify Subscription</Button>}
+        
         </Box>
 
-
         <Box mt={4}>
+
           <Typography variant="h6" gutterBottom>
-            Members
+            Team Members
           </Typography>
-          <Typography>You can manage roles of your team. There must always be one Owner of the team.</Typography>
+
+          <Typography variant="h6" gutterBottom>
+            Current Team Members
+          </Typography>
+
+          <Typography className="description">
+            <p>This table shows all active team members and their roles. Admins or owners can modify the roles of an existing user. There must always be one Owner of the team.</p>
+            <p>Members can create API Keys to access the API. Admin users can modify the team settings (including the name, subscription, or adding/removing members to/from the team). Owners inherit all Admin permissions and can also delete the team</p>
+          </Typography>
+
           <TableContainer component={Paper}>
             <Table>
               <TableHead>
                 <TableRow>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Role</TableCell>
-                  <TableCell></TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Email</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Role</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Available Actions</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -507,19 +547,23 @@ function Team() {
           </TableContainer>
         </Box>
 
-
         <Box mt={4}>
+
           <Typography variant="h6" gutterBottom>
-            Invited Users
+            Pending Invites
           </Typography>
+          <Typography className="description">
+            <p>This shows invitations sent out that are yet to be responded to. Once an invitation is accepted or rejected by the user, or revoked by a team admin or owner it will disappear from this table.</p>
+          </Typography>
+
           <TableContainer component={Paper}>
             <Table>
               <TableHead>
                 <TableRow>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Role</TableCell>
-                  <TableCell>Date Last Invite Sent</TableCell>
-                  <TableCell>Actions</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Email</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Role</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Date Last Invite Sent</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Available Actions</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -556,7 +600,7 @@ function Team() {
                 ) : (
                   <TableRow>
                     <TableCell colSpan={4} align="center">
-                      No invited users
+                      No pending invites
                     </TableCell>
                   </TableRow>
                 )}
@@ -566,11 +610,21 @@ function Team() {
         </Box>
 
         <br />
-        <Typography>Invite User</Typography>
-        <Typography>Members can subscribe/unsubscribe to feeds, view their posts, and access the API. Admins inherit all member permissions but can also add/remove users from the team and edit the team information (including subscriptions). Owners inherit all Admin permissions and can also delete the team.</Typography>
-        {id && <InviteUserList
-          noOfFreeSlots={(activeTeam?.user_limit || 0) - (activeTeam?.members_count || 0) - (activeTeam?.invitations_count || 0)}
-          isOwner={true} teamId={id} onComplete={() => { reloadInvitationList() }}></InviteUserList>}
+        {activeUserIsAdmin && (<>
+          <Typography variant="h6" gutterBottom>
+            Invite a New Team Member
+          </Typography>
+          <Typography className="description">
+            <p>Members can create API Keys to access the API. Admin users can modify the team settings (including the name, subscription, or adding/removing members to/from the team). Owners inherit all Admin permissions and can also delete the team.</p>
+          </Typography>
+
+          {activeTeam && <InviteUserList
+            isOwner={true}
+            teamId={activeTeam.id}
+            onComplete={() => { reloadInvitationList() }}
+            noOfFreeSlots={activeTeam?.user_limit - activeTeam?.members_count - activeTeam?.invitations_count}
+          ></InviteUserList>}
+        </>)}
       </Box>
 
       <Box>
